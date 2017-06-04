d7388a245763dada2199343ea1da2368a8755303
imenger <imenger@veleri.hr>
2017-06-04 12:06:08 +0200

    CRUD za radnike

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,29 @@ Route::get('/stroj/{id}', 'StrojController@show');
 Route::post('/stroj', 'StrojController@store');
 Route::post('/stroj/{id}', 'StrojController@update');
 
+Route::get('/proizvodi', 'ProizvodController@index');
+Route::get('/proizvod', 'ProizvodController@create');
+Route::get('/proizvod/{id}', 'ProizvodController@show');
+Route::post('/proizvod', 'ProizvodController@store');
+Route::post('/proizvod/{id}', 'ProizvodController@update');
 
+Route::get('/radnici', 'RadnikController@index');
+Route::get('/radnik', 'RadnikController@create');
+Route::get('/radnik/{id}', 'RadnikController@show');
+Route::post('/radnik', 'RadnikController@store');
+Route::post('/radnik/{id}', 'RadnikController@update');
+
+Route::get('/skladista', 'SkladisteController@index');
+Route::get('/skladiste', 'SkladisteController@create');
+Route::get('/skladiste/{id}', 'SkladisteController@show');
+Route::post('/skladiste', 'SkladisteController@store');
+Route::post('/skladiste/{id}', 'SkladisteController@update');
+
+Route::get('/izvjesca', 'IzvjesceController@index');
+Route::get('/izvjesce', 'IzvjesceController@create');
+Route::get('/izvjesce/{id}', 'IzvjesceController@show');
+Route::post('/izvjesce', 'IzvjesceController@store');
+Route::post('/izvjesce/{id}', 'IzvjesceController@update');
 
 
 Auth::routes();
