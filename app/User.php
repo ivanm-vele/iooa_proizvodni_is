d773427030f9361850ecc7f5d62f2c918430bf98
imenger <imenger@veleri.hr>
2017-06-05 11:53:50 +0200

    korisnici se sad pravilno spremaju

--- app/User.php
+++ app/User.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
 
+    public $timestamps = false;
+
     /**
      * The table associated with the model.
      *
